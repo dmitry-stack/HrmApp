47c71b262888620a1a6691ba51e6d2e8a8965ff6
Dmitry Hais <hadminsk@icloud.com>
2026-09-01 14:58:28 +0200

    feat: router setup

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,106 @@
+import { createRoute, redirect } from '@tanstack/react-router';
+import { DealsPage } from '@pages/deals/DealsPage';
+import { Route as rootRoute } from './__route';
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  beforeLoad: () => {
+    throw redirect({ to: '/deals' });
+  },
+});
+
+const dealsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/deals',
+  component: DealsPage,
+});
+
+const canditatesRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/candidates',
+  component: () => <div>Candidates</div>,
+});
+
+const profileRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/profile',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+const briefcaseRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/briefcase',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+const envelopeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/envelope',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+const pigRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/pig',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+const calendarRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/calendar',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+const diagramRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/diagram',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+const settingsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/settings',
+  component: () => (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This Page is in development</h1>
+    </div>
+  ),
+});
+
+export const routeTree = rootRoute.addChildren([
+  indexRoute,
+  dealsRoute,
+  canditatesRoute,
+  profileRoute,
+  briefcaseRoute,
+  envelopeRoute,
+  pigRoute,
+  calendarRoute,
+  diagramRoute,
+  settingsRoute,
+]);
--- src/widgets/navbar/Navbar.tsx
+++ src/widgets/navbar/Navbar.tsx
@@ -3,48 +3,61 @@ import briefcase from '@shared/assets/navbar/briefcase.svg';
 import calendar from '@shared/assets/navbar/calendar.svg';
 import diagram from '@shared/assets/navbar/diagram.svg';
 import envelope from '@shared/assets/navbar/envelope.svg';
-import group from '@shared/assets/navbar/group.svg';
+import candidates from '@shared/assets/navbar/candidates.svg';
 import pig from '@shared/assets/navbar/pig.svg';
 import profile from '@shared/assets/navbar/profile.svg';
 import settings from '@shared/assets/navbar/settings.svg';
-import visuals from '@shared/assets/navbar/visuals.svg';
+import deals from '@shared/assets/navbar/deals.svg';
+import { Link } from '@tanstack/react-router';
 
-const topNavItems = [
-  { icon: visuals, label: 'Visuals' },
-  { icon: group, label: 'Group' },
-  { icon: profile, label: 'Profile' },
-  { icon: briefcase, label: 'Briefcase' },
-  { icon: envelope, label: 'Envelope' },
-  { icon: pig, label: 'Pig' },
-  { icon: calendar, label: 'Calendar' },
+type NavItem = {
+  icon: string;
+  label: string;
+  link: string;
+};
+const topNavItems: NavItem[] = [
+  { icon: deals, label: 'Deals', link: '/deals' },
+  { icon: candidates, label: 'Candidates', link: '/candidates' },
+  { icon: profile, label: 'Profile', link: '/profile' },
+  { icon: briefcase, label: 'Briefcase', link: '/briefcase' },
+  { icon: envelope, label: 'Envelope', link: '/envelope' },
+  { icon: pig, label: 'Pig', link: '/pig' },
+  { icon: calendar, label: 'Calendar', link: '/calendar' },
 ];
 
-const bottomNavItems = [
-  { icon: diagram, label: 'Diagram' },
-  { icon: settings, label: 'Settings' },
+const bottomNavItems: NavItem[] = [
+  { icon: diagram, label: 'Diagram', link: '/diagram' },
+  { icon: settings, label: 'Settings', link: '/settings' },
 ];
 
-const purpleIconFilter =
+const purpleIconFilter: string =
   'brightness(0) saturate(100%) invert(44%) sepia(31%) saturate(973%) hue-rotate(200deg) brightness(95%) contrast(90%)';
 
 export function Navbar() {
   const [hoveredItem, setHoveredItem] = useState<string | null>(null);
   const [activeItem, setActiveItem] = useState<string | null>(null);
 
-  const renderButton = (icon: string, label: string) => {
+  const renderButton = (icon: string, label: string, link: string) => {
     const isHovered = hoveredItem === label;
     const isActive = activeItem === label;
 
     return (
-      <button
+      <Link
         key={label}
-        type="button"
+        to={link}
         aria-label={label}
         onMouseEnter={() => setHoveredItem(label)}
         onMouseLeave={() => setHoveredItem(null)}
-        onClick={() => setActiveItem(label)}
         className="flex h-12 w-12 items-center justify-center rounded-md bg-transparent transition-all"
-        style={{ backgroundColor: isHovered || isActive ? '#707FDD1A' : 'transparent' }}
+        style={{
+          backgroundColor: isHovered ? '#707FDD1A' : 'transparent',
+        }}
+        activeProps={{
+          style: {
+            backgroundColor: '#707FDD1A',
+          },
+        }}
+        onClick={() => setActiveItem(label)}
       >
         <img
           src={icon}
@@ -52,18 +65,18 @@ export function Navbar() {
           className="h-5 w-5"
           style={{ filter: isActive ? purpleIconFilter : 'none' }}
         />
-      </button>
+      </Link>
     );
   };
 
   return (
     <nav className="flex h-full w-18 flex-col items-center border-r border-[#F4F4F4] bg-white px-3 py-5 pr-4 sm:gap-6 lg:gap-8">
       <div className="flex flex-col items-center gap-4">
-        {topNavItems.map(({ icon, label }) => renderButton(icon, label))}
+        {topNavItems.map(({ icon, label, link }) => renderButton(icon, label, link))}
       </div>
 
       <div className="mt-auto mb-8 flex flex-col items-center gap-4 ">
-        {bottomNavItems.map(({ icon, label }) => renderButton(icon, label))}
+        {bottomNavItems.map(({ icon, label, link }) => renderButton(icon, label, link))}
       </div>
     </nav>
   );
